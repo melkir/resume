6eb394f3a8ce42972908da907797832b52641cc0
melkir <melkir13@gmail.com>
2018-01-19 23:08:19 +0100

    :rocket: First version

---
+++ resume.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"os/exec"
+	"strings"
+	"text/template"
+	"time"
+)
+
+// JSON type structure
+type JSON map[string]interface{}
+
+func main() {
+	funcMap := template.FuncMap{
+		"month":  formatAsMonth,
+		"year":   formatAsYear,
+		"toList": formatArrayAsList,
+	}
+
+	const jsonFile = "data.json"
+	jsondata, err := ioutil.ReadFile(jsonFile)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	const templateFile = "template.tex"
+	t, err := template.New(templateFile).Funcs(funcMap).Delims("[[", "]]").ParseFiles(templateFile)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	resume := JSON{}
+	if err := json.Unmarshal(jsondata, &resume); err != nil {
+		panic(err)
+	}
+
+	if _, err := os.Stat("out"); os.IsNotExist(err) {
+		os.Mkdir("out", 0755)
+	}
+
+	file, err := os.Create("out/resume.tex")
+	if err != nil {
+		log.Fatal("Cannot create file", err)
+	}
+	defer file.Close()
+
+	if err := t.Execute(file, resume); err != nil {
+		panic(err)
+	}
+
+	if err := exec.Command("pdflatex", "-output-directory", "out", "out/resume.tex").Run(); err != nil {
+		log.Fatal("Cannot create pdf", err)
+	}
+}
+
+func formatAsMonth(date string) string {
+	return formatAsDate(date, "Jan")
+}
+
+func formatAsYear(date string) string {
+	return formatAsDate(date, "2006")
+}
+
+func formatAsDate(date string, format string) string {
+	t, err := time.Parse("2006-01-02", date)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return t.Format(format)
+}
+
+func formatArrayAsList(elements []interface{}) string {
+	return strings.Trim(strings.Replace(fmt.Sprint(elements), " ", ", ", -1), "[]") + "."
+}
